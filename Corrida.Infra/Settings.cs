04afd5d0328afe4c4aa044ff20512a1e43191c8d
Alipio F. Ferro <30441950+Alipio2@users.noreply.github.com>
2020-08-17 17:14:59 -0300

    Update Settings.cs

--- Corrida.Infra/Settings.cs
+++ Corrida.Infra/Settings.cs
@@ -2,6 +2,6 @@
 {
     public static class Settings
     {
-        public static string ConnectionStringAlp = @"Server=srvbatman.database.windows.net,1433;Database=BatmanBd;User ID=alipio;Password=@lp101010";
+        public static string ConnectionStringAlp = Server=srvbatman.database.windows.net,1433;Database=BatmanBd;User ID=alipio;Password=2
     }
 }
